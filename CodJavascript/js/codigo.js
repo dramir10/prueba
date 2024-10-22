875e41b075a98a12b4c1e228aa00b4e0df175f65
Diego <dh.ramirez15@hotmail.com>
2024-10-21 21:46:49 -0300

    Creando un Javascript

---
+++ CodJavascript/js/codigo.js
@@ -0,0 +1,12 @@
+alert("Hola mundo desde javascript")
+
+//Esto es un comentario
+
+//var
+let nombre = prompt("Ingrese su nombre: ")  // nombre = root
+if (nombre == "root") {
+	alert("Acceso correcto")
+}
+else {
+	alert("Acceso denegado")
+}
